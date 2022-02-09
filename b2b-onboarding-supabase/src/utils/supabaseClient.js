82aa048daca463e5c0168bc5e20de1d56e816637
Oigen43 <oigen43@icloud.com>
2022-02-09 17:03:35 +0300

    init deploy

--- b2b-onboarding-supabase/src/utils/supabaseClient.js
+++ b2b-onboarding-supabase/src/utils/supabaseClient.js
@@ -1,7 +1,5 @@
+import config from 'config';
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQzODE4NjM5LCJleHAiOjE5NTkzOTQ2Mzl9.bGdgi5Za2S49VW0mTfuCFsJYn1jEaUDPZ26BO0IHBio';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(config.supabase.url, config.supabase.anonKey);
---
+++ deploy/bin/deploy-dependencies.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd "$( dirname "${BASH_SOURCE[0]}" )"/../
+
+sh ./dependencies/ingress-nginx/bin/deploy-ingress-nginx.sh
+sh ./dependencies/regcred/bin/create-docker-regcred.sh
